282695c8fcf74716ee4376a905d90adcebfce552
Udayrakesh <Uday.raksh@gmail.com>
2015-03-28 00:09:19 -0700

    Update SQLFunctions.py

--- CiaoCrawler/SQLFunctions.py
+++ CiaoCrawler/SQLFunctions.py
@@ -31,3 +31,13 @@ def add_user_buddy(conn, user_id, username, buddy_id, buddy_name):
         #print(query);
         SQLConnect.mysql_execute(conn,query)
         return conn
+
+def create_prod(conn):
+    cur=conn.cursor();
+    cur.execute('create table if not exists product(prod_id bigint(20) unsigned, prod_name varchar(150),product_category varchar(150),product_subcategory varchar(150), primary key(prod_id))ENGINE=InnoDB DEFAULT CHARSET=latin1')
+    cur.execute('commit')
+
+def add_product(conn,id,name,category,s_category):
+    cur=conn.cursor()
+    cur.execute("insert into product values("+id+",'"+name+"','"+category+"','"+s_category+"')")
+    cur.execute('commit')
